cefe877e21154534b9ce52b5a13cdfb0428b1bc4
Jin <minihyojin@gmail.com>
2017-03-02 22:58:06 -0600

    Update triplets.js

--- triplets.js
+++ triplets.js
@@ -1,7 +1,24 @@
 // a input is 5 6 7
 //b input is 3 6 10
 
+//'5 6 7'.split(' ') => ['5', '6', '7']
+//'5 6 7'.split(' ').map(i => parseInt(i)); => [5, 6, 7]
 
+
+var alice = 0
+var bob = 0
+var a='5 6 7'.split(' ').map(i => parseInt(i));
+var b='3 6 10'.split(' ').map(i => parseInt(i));
 function main(a, b){
-  
+  for (var i=0; i<=2; i++){
+        if(a[i]>b[i]){
+            alice++;
+        }else if (a[i]<b[i]){
+            bob++;
+        }
+    }
+    console.log(alice+' '+bob);
 }
+}
+
+//output 1 1
